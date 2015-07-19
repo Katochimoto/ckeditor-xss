7c9e5595852b0d40b7d8ed2ac1e8f1186f27d260
Katochimoto <bufpost@yandex.ru>
2015-07-19 11:43:06 +0300

    init

---
+++ plugin.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+
+    var REG_JAVASCRIPT_HREF = /^\s*javascript:/i;
+
+    CKEDITOR.plugins.add('xss', {
+        modes: { 'wysiwyg': 1 },
+
+        /**
+         * @this {Editor}
+         */
+        afterInit: function(editor) {
+            var rules = {
+                'attributes': {
+                    'href': function(value, element) {
+                        if (REG_JAVASCRIPT_HREF.test(decodeURIComponent(value))) {
+                            delete element.attributes.href;
+                            delete element.attributes['data-cke-saved-href'];
+                            return '';
+                        }
+                    }
+                }
+            };
+
+            editor.dataProcessor.dataFilter.addRules(rules, { 'applyToAll': true, 'priority': 0 });
+        }
+    });
+}());
